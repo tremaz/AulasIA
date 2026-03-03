bc29c3df806fd792cd6c72352d924886a4570b54
tremaz <diegooliveirarocha66@gmail.com>
2026-03-03 12:43:50 -0300

    update besta

--- RegressãoLinearMultipla.py
+++ RegressãoLinearMultipla.py
@@ -1,10 +1,9 @@
 import numpy as np
 
 x = np.array([[2, 50], [8, 110], [11, 120], [10, 550], [8, 295], [4, 200], [2, 375], [2, 52], [9, 100], [8, 300]])
-xt = x.transpose()
-    
 
-P = x*xt
+xDet = np.linalg.det(x)
 
-print(x)
-print(xt)
+y = np.array([[10, 1], [1, 1], [1, 1], [1, 1], [1, 1]])
+
+print(x.T)
